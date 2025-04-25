48d0128cfeb948f799fe8409ccf909be853fcea8
Gustavo Hennig <gustavohe@gmail.com>
2025-04-25 14:53:13 -0300

    Adding browse button

--- SimpleMonitorTools/ShortcutManagerWindow.axaml.cs
+++ SimpleMonitorTools/ShortcutManagerWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using System.Threading.Tasks;
 
 namespace SimpleMonitorTools
 {
@@ -9,6 +10,20 @@ namespace SimpleMonitorTools
         {
             InitializeComponent();
             DataContext = new ShortcutManagerViewModel();
+        }
+
+        private async void BrowseButton_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog();
+            var result = await dialog.ShowAsync(this);
+
+            if (result != null && result.Length > 0)
+            {
+                if (DataContext is ShortcutManagerViewModel viewModel)
+                {
+                    viewModel.NewShortcutPath = result[0];
+                }
+            }
         }
     }
 }
